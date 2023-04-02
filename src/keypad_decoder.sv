950868c42054f923c6b2b92b8467f6854d83e238
Clayton Lawton <cla10law10@gmail.com>
2023-04-02 16:15:08 -0700

    Keypad Attempt 1

--- src/keypad_decoder.sv
+++ src/keypad_decoder.sv
@@ -4,7 +4,7 @@ module keypad_decoder(
     input clk,
     input rst,
     input [3:0] row,
-    output [3:0] col,
+    output logic [3:0] col,
     output [3:0] decode_out,
     output is_a_key_pressed
   );
@@ -19,36 +19,198 @@ module keypad_decoder(
     begin
       decode_reg = 4'b0;
       is_a_key_pressed_reg = 1'b0;
+      sclk = 'b0;
     end
     else
     begin
-        // 2ms
-        if sclk = 20'd100000 begin
-            col = 4'b0111;
-            sclk = sclk + 1;
+      //////////////////////////////////////
+      //          Scan Column 1           //
+      //////////////////////////////////////
+      // 2ms
+      if (sclk == 20'd100000)
+      begin
+        // Column 1
+        col = 4'b0111;
+        sclk = sclk + 1;
+      end
+      // Check row pins
+      else if (sclk == 20'd100008)
+      begin
+        // Row 1
+        if (row == 4'b0111)
+        begin
+          decode_reg = 4'b0001; //1
+          is_a_key_pressed_reg = 1'b1;
         end
-        else if (sclk == 20'b100008) begin
-            if (row == 4'b0111) begin
-                decode_reg = 4'b0001; //1
-                is_a_key_pressed_reg = 1'b1;
-            end
-            if (row == 4'b1011) begin
-                decode_reg = 4'b0100; //4
-                is_a_key_pressed_reg = 1'b1;
-            end
-            if (row == 4'b1101) begin
-                decode_reg = 4'b0111; //7
-                is_a_key_pressed_reg = 1'b1;
-            end
-            if (row == 4'b1110) begin
-                decode_reg = 4'b0000; //0
-                is_a_key_pressed_reg = 1'b1;
-            end
+        // Row 2
+        else if (row == 4'b1011)
+        begin
+          decode_reg = 4'b0100; //4
+          is_a_key_pressed_reg = 1'b1;
         end
-    end // end clk
-  end // end always_ff
+        // Row 3
+        else if (row == 4'b1101)
+        begin
+          decode_reg = 4'b0111; //7
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 4
+        else if (row == 4'b1110)
+        begin
+          decode_reg = 4'b0000; //0
+          is_a_key_pressed_reg = 1'b1;
+        end
+        else
+        begin
+          decode_reg = decode_reg;
+          is_a_key_pressed_reg = 0;
+        end
 
-  assign is_a_key_pressed = is_a_key_pressed_reg;
-  assign decode_out = decode_reg;
+        sclk = sclk + 1;
+      end
+      //////////////////////////////////////
+      //          Scan Column 2           //
+      //////////////////////////////////////
+
+      else if (sclk == 20'd200000)
+      begin
+        // Column 2
+        col = 4'b1011;
+        sclk = sclk + 1;
+      end
+      // Check row pins
+      else if (sclk == 20'd200008)
+      begin
+        // Row 1
+        if (row == 4'b0111)
+        begin
+          decode_reg = 4'b0010; // 2
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 2
+        else if (row == 4'b1011)
+        begin
+          decode_reg = 4'b0101; // 5
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 3
+        else if (row == 4'b1101)
+        begin
+          decode_reg = 4'b1000; // 8
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 4
+        else if (row == 4'b1110)
+        begin
+          decode_reg = 4'b1111; // F
+          is_a_key_pressed_reg = 1'b1;
+        end
+        else
+        begin
+          decode_reg = decode_reg;
+          is_a_key_pressed_reg = 0;
+        end
+
+        sclk = sclk + 1;
+      end
+      //////////////////////////////////////
+      //          Scan Column 3           //
+      //////////////////////////////////////
+
+      else if (sclk == 20'd300000)
+      begin
+        // Column 3
+        col = 4'b1101;
+        sclk = sclk + 1;
+      end
+      // Check row pins
+      else if (sclk == 20'd300008)
+      begin
+        // Row 1
+        if (row == 4'b0111)
+        begin
+          decode_reg = 4'b0011; // 3
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 2
+        else if (row == 4'b1011)
+        begin
+          decode_reg = 4'b0110; // 6
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 3
+        else if (row == 4'b1101)
+        begin
+          decode_reg = 4'b1001; // 9
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 4
+        else if (row == 4'b1110)
+        begin
+          decode_reg = 4'b1110; // E
+          is_a_key_pressed_reg = 1'b1;
+        end
+        else
+        begin
+          decode_reg = decode_reg;
+          is_a_key_pressed_reg = 0;
+        end
+
+        sclk = sclk + 1;
+      end
+      //////////////////////////////////////
+      //          Scan Column 4           //
+      //////////////////////////////////////
+
+      else if (sclk == 20'd400000)
+      begin
+        // Column 4
+        col = 4'b1011;
+        sclk = sclk + 1;
+      end
+      // Check row pins
+      else if (sclk == 20'd400008)
+      begin
+        // Row 1
+        if (row == 4'b0111)
+        begin
+          decode_reg = 4'b1010; // A
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 2
+        else if (row == 4'b1011)
+        begin
+          decode_reg = 4'b1011; // b
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 3
+        else if (row == 4'b1101)
+        begin
+          decode_reg = 4'b1100; // C
+          is_a_key_pressed_reg = 1'b1;
+        end
+        // Row 4
+        else if (row == 4'b1110)
+        begin
+          decode_reg = 4'b1101; // d
+          is_a_key_pressed_reg = 1'b1;
+        end
+        else
+        begin
+          decode_reg = decode_reg;
+          is_a_key_pressed_reg = 0;
+        end
+
+        sclk = 'b0;
+      end
+    else
+    begin
+      sclk = sclk + 1;
+    end
+  end // end clk
+end // end always_ff
+
+assign is_a_key_pressed = is_a_key_pressed_reg;
+assign decode_out = decode_reg;
 
 endmodule
